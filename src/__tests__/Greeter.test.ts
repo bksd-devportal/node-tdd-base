a59c161d6385d38a61a7e83496885bd2e6c3a5ec
Suren Rodrigo <bksdrodrigo@outlook.com>
2021-06-05 07:39:04 +0530

    did uniform rename

--- src/__tests__/Greeter.test.ts
+++
@@ -1,7 +0,0 @@
-import { Greeter } from '../index';
-
-describe('My Greeter Tests', () => {
-  test('My Greeter', () => {
-    expect(Greeter('Carl')).toBe('Hello Carl Welcome to Node!!');
-  });
-});
